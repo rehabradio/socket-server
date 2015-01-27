c31fdceb6ae2b41243c0df0691e2cdb7f44fc231
mark mcconnell <mark@rehabstudio.com>
2015-01-27 15:07:52 +0000

    login method updated

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ import redis
 import requests
 
 from flask import Flask
-from flask import render_template
+from flask import jsonify
 from flask import session
-from flask.ext.login import LoginManager
+from flask import request
+from flask import render_template
 from flask.ext.socketio import SocketIO, emit
 from threading import Thread
 
@@ -17,52 +18,16 @@ from gevent import monkey
 
 
 monkey.patch_all()
-login_manager = LoginManager()
 
 
 app = Flask(__name__)
 app.debug = os.environ.get('DEBUG', False)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret!')
 
-login_manager.init_app(app)
-
 REDIS_URL = os.environ.get('REDISCLOUD_URL')
 redis = redis.from_url(REDIS_URL)
 
 socketio = SocketIO(app)
-
-
-@login_manager.request_loader
-def load_user_from_request(request):
-    """Log a user in, using a valid google oauth token, with valid associated email.
-    """
-    # Retrieve the access token from the request header
-    access_token = request.headers.get('X-Google-Auth-Token')
-
-    if access_token:
-        # Validated the token and pull down the user details
-        params = {'alt': 'json', 'access_token': access_token}
-        r = requests.get(
-            'https://www.googleapis.com/oauth2/v1/userinfo',
-            params=params
-        )
-        person = r.json()
-
-        # Ensure a valid json object is returned
-        if person.get('error') or person['verified_email'] is False:
-            return None
-
-        # Retrieve the whitelisted domains set in the .env file
-        white_listed_domains = os.environ.get('GOOGLE_WHITE_LISTED_DOMAINS', '').split(',')
-
-        # Ensure the users domain exists within the whilelist
-        if (person.get('hd') and person.get('hd') not in white_listed_domains):
-            return None
-
-        session['user'] = person
-
-        return person
-    return None
 
 
 def playlist_thread():
@@ -110,6 +75,48 @@ def index():
     """Endpoint for the playlist and queue updates.
     """
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    """Log a user in, using a valid google oauth token, with valid associated email.
+    """
+    data = {'code': 403}
+
+    # Retrieve the access token from the request header
+    access_token = request.headers.get('X-Google-Auth-Token')
+
+    if not access_token:
+        data['message'] = 'X-Google-Auth-Token not found'
+    else:
+        # Validated the token and pull down the user details
+        params = {'alt': 'json', 'access_token': access_token}
+        r = requests.get(
+            'https://www.googleapis.com/oauth2/v1/userinfo',
+            params=params
+        )
+        person = r.json()
+
+        # Ensure a valid json object is returned
+        if person.get('error'):
+            return jsonify(person.get('error'))
+
+        if person['verified_email'] is False:
+            data['message'] = 'email not verified'
+            return jsonify(data)
+
+        # Retrieve the whitelisted domains set in the .env file
+        white_listed_domains = os.environ.get('GOOGLE_WHITE_LISTED_DOMAINS', '').split(',')
+
+        # Ensure the users domain exists within the whilelist
+        if (person.get('hd') and person.get('hd') not in white_listed_domains):
+            data['message'] = 'Email domain not whitelisted.'
+            return jsonify(data)
+
+        session['user'] = person
+        data = {'code': 200}
+
+    return jsonify(data)
 
 
 @socketio.on('connect', namespace='/updates')
