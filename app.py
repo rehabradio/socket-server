269b9542df2b9f476156118a905b83018245daa0
mark mcconnell <mark@rehabstudio.com>
2015-01-28 12:06:00 +0000

    socket auth temp removed

--- app.py
+++ app.py
@@ -126,18 +126,18 @@ def login():
 def connect():
     """Starts reporting the threads.
     """
-    app.logger.info('session: {0}'.format(session))
-    if session.get('user'):
-        emit(
-            'my response',
-            {'data': '{0} has joined'.format(session['user']['name'])}
-        )
-        pthread = Thread(target=playlist_thread)
-        pthread.start()
-        qthread = Thread(target=queue_thread)
-        qthread.start()
-    else:
-        emit('error', {'code': '403'})
+    # app.logger.info('session: {0}'.format(session))
+    # if session.get('user'):
+    #     emit(
+    #         'my response',
+    #         {'data': '{0} has joined'.format(session['user']['name'])}
+    #     )
+    pthread = Thread(target=playlist_thread)
+    pthread.start()
+    qthread = Thread(target=queue_thread)
+    qthread.start()
+    # else:
+    #     emit('error', {'code': '403'})
 
 
 if __name__ == '__main__':
