3d9bd2b314c948b1aa09412be527b83a40854b61
mark mcconnell <mark@rehabstudio.com>
2015-01-28 17:44:18 +0000

    login removed (backed up to app_auth.py)

--- app_auth.py
+++ app_auth.py
@@ -7,6 +7,8 @@ import redis
 import requests
 
 from flask import Flask
+from flask import jsonify
+from flask import session
 from flask import request
 from flask.ext.cors import CORS
 from flask.ext.socketio import SocketIO, emit
@@ -21,6 +23,7 @@ monkey.patch_all()
 app = Flask(__name__)
 app.debug = os.environ.get('DEBUG', False)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret!')
+
 
 CORS(app, allow_headers=['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'X_GOOGLE_AUTH_TOKEN'])
 
@@ -70,13 +73,64 @@ def queue_thread():
                 socketio.emit(label, namespace=namespace)
 
 
+@app.route('/login')
+def login():
+    """Log a user in, using a valid google oauth token, with valid associated email.
+    """
+    if session.get('user'):
+        return jsonify({'code': 200, 'message': session['user']})
+
+    data = {'code': 403}
+
+    # Retrieve the access token from the request header
+    access_token = request.headers.get('X-Google-Auth-Token')
+
+    if not access_token:
+        data['message'] = 'X-Google-Auth-Token not found'
+    else:
+        # Validated the token and pull down the user details
+        params = {'alt': 'json', 'access_token': access_token}
+        r = requests.get(
+            'https://www.googleapis.com/oauth2/v1/userinfo',
+            params=params
+        )
+        person = r.json()
+
+        # Ensure a valid json object is returned
+        if person.get('error'):
+            return jsonify(person.get('error'))
+
+        if person['verified_email'] is False:
+            data['message'] = 'email not verified'
+            return jsonify(data)
+
+        # Retrieve the whitelisted domains set in the .env file
+        white_listed_domains = os.environ.get('GOOGLE_WHITE_LISTED_DOMAINS', '').split(',')
+
+        # Ensure the users domain exists within the whilelist
+        if (person.get('hd') and person.get('hd') not in white_listed_domains):
+            data['message'] = 'Email domain not whitelisted.'
+            return jsonify(data)
+
+        session['user'] = person
+        data = {'code': 200, 'message': session['user']}
+
+    return jsonify(data)
+
+
 @socketio.on('connect', namespace='/updates')
 def connect():
     """Starts reporting the threads.
     """
-    emit('connected', {'data': 'Connected'})
-    Thread(target=playlist_thread).start()
-    Thread(target=queue_thread).start()
+    if session.get('user'):
+        emit(
+            'my response',
+            {'data': '{0} has joined'.format(session['user']['name'])}
+        )
+        Thread(target=playlist_thread).start()
+        Thread(target=queue_thread).start()
+    else:
+        emit('error', {'code': '403'})
 
 
 if __name__ == '__main__':
